2d37dcac1607655360c7f7216413fa3ee69d3285
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-12-11 16:22:51 +0900

    Finish up to brush-size & fill, paint button

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const colors = document.getElementsByClassName("jsColor");
+const controls = document.getElementById("jsControls");
+const mode = document.getElementById("jsMode");
+const range = document.getElementById("jsRange");
 
 const ctx = canvas.getContext("2d");
 
@@ -7,6 +10,7 @@ ctx.lineWidth = 2.5;
 ctx.strokeStyle = "#2d3436";
 
 let isPainting = false;
+let isFilling = false;
 
 const stopPainting = () => {
   isPainting = false;
@@ -33,16 +37,40 @@ const handleClickedColor = event => {
   ctx.strokeStyle = color;
 };
 
-const init = () => {
+const handleRange = event => {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+};
+
+const handleMode = () => {
+  if (!isFilling) {
+    isFilling = true;
+    mode.innerText = "Paint";
+  } else {
+    isFilling = false;
+    mode.innerText = "Fill";
+  }
+};
+
+const canvasInit = () => {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+};
+
+const controlsInit = () => {
   Array.from(colors, color =>
     color.addEventListener("click", handleClickedColor)
   );
+  range.addEventListener("input", handleRange);
+  mode.addEventListener("click", handleMode);
 };
 
 if (canvas) {
-  init();
+  canvasInit();
+}
+
+if (controls) {
+  controlsInit();
 }
